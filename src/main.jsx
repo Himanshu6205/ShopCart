8ab7f00d4d6325fca7235a31889c422e6db41009
Himanshu6205 <7himanshu69@gmail.com>
2024-10-26 02:49:08 +0530

    fifth

--- src/main.jsx
+++ src/main.jsx
@@ -15,8 +15,8 @@ import "././assets/css/animate.css";
 import "././assets/css/style.min.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Blog from "../src/Blog.jsx";
-import Home from "../src/home.jsx";
+import Blog from "./Blog.jsx";
+import Home from "./home.jsx";
 
 const router = createBrowserRouter([
   {
